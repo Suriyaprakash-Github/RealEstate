e5734df8ffa63555f5d43e9ba5156d952e70850c
Suriyaprakash-Github <yoursgump@gmail.com>
2023-04-28 23:48:55 +0530

    hero completed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Hero.css";
+import { HiLocationMarker } from "react-icons/hi";
+import CountUp from "react-countup";
 
 const Hero = () => {
   return (
     <>
       <section className="hero-wrapper">
-        <div className="paddings innerWidth flexCenter hero-container ">
+        <div className=" innerWidth flexCenter paddings hero-container ">
           {/* left side */}
           <div className="flexColStart hero-left">
-            <div className="hero-title">
+            <div className=" hero-title">
               <div className="orange-circle"></div>
               <h1>
                 Discover <br />
@@ -16,7 +18,7 @@ const Hero = () => {
               </h1>
             </div>
 
-            <div className="flexColStart hero-des">
+            <div className=" flexColStart secondaryText hero-des">
               <span>
                 find a variety of properties that suit you very easily
               </span>
@@ -25,7 +27,37 @@ const Hero = () => {
               </span>
             </div>
 
-            <div className="search-bar">Search Bar</div>
+            <div className="flexCenter paddings search-bar">
+              <HiLocationMarker color="var(--blue)" size={25} />
+              <input type="text" />
+              <button className="button">Search</button>
+            </div>
+
+            <div className="flexCenter stats">
+              <div className="flexColCenter stat">
+                <span>
+                  <CountUp start={8800} end={9000} duration={4} />
+                  <span>+</span>
+                </span>
+                <span className="secondaryText">Premium Products</span>
+              </div>
+
+              <div className="flexColCenter stat">
+                <span>
+                  <CountUp start={1950} end={2000} duration={4} />
+                  <span>+</span>
+                </span>
+                <span className="secondaryText">Happy Customers</span>
+              </div>
+
+              <div className="flexColCenter stat">
+                <span>
+                  <CountUp end={20} />
+                  <span>+</span>
+                </span>
+                <span className="secondaryText">Award Winnings</span>
+              </div>
+            </div>
           </div>
 
           <div className="flexCenter hero-right">
