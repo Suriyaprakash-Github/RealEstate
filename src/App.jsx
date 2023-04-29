86db8cd2136820bdbf49c2b752b6ff7d3ba9ded1
Suriyaprakash-Github <yoursgump@gmail.com>
2023-04-29 23:47:39 +0530

    getstarted completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Companies from "./components/Companies/Companies";
 import Contact from "./components/Contact/Contact";
+import GetStarted from "./components/GetStarted/GetStarted";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Residencies from "./components/Residencies/Residencies";
@@ -14,6 +15,7 @@ function App() {
       <Residencies />
       <Value />
       <Contact />
+      <GetStarted />
     </div>
   );
 }
